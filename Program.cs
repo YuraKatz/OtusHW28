fa89dd34f2c37639f5a3720bc028ac99493a1395
YuriKatzL <54262627+YuriKatzL@users.noreply.github.com>
2021-03-23 06:23:50 +0200

    test fish

--- Program.cs
+++ Program.cs
@@ -6,24 +6,33 @@ namespace OtusHW28
     {
         private static void Main(string[] args)
         {
-            var animal = new Animal("animal");
-          
-            Console.WriteLine($"Original Item :{ Environment.NewLine}{animal}");
-            Console.WriteLine($"Cloned Item1 :{ Environment.NewLine}{animal.CloneV2()}");
-     
+
 
             var fish = new Fish(true, "animal2");
-
+            var val = fish.CloneV2();
             Console.WriteLine($"Original Item :{ Environment.NewLine}{fish}");
-            Console.WriteLine($"Cloned Item1 :{ Environment.NewLine}{fish.CloneV2()}");
-          
+            Console.WriteLine($"Cloned Item1 :{ Environment.NewLine}{val}");
 
 
-            var goldfish = new GoldFish(3, "animal3");
 
-            Console.WriteLine($"Original Item :{ Environment.NewLine}{goldfish}");
-            Console.WriteLine($"Cloned Item1 :{ Environment.NewLine}{goldfish.CloneV2()}");
-        
+            //var animal = new Animal("animal");
+
+            //Console.WriteLine($"Original Item :{ Environment.NewLine}{animal}");
+            //Console.WriteLine($"Cloned Item1 :{ Environment.NewLine}{animal.CloneV2()}");
+
+
+            //var fish = new Fish(true, "animal2");
+
+            //Console.WriteLine($"Original Item :{ Environment.NewLine}{fish}");
+            //Console.WriteLine($"Cloned Item1 :{ Environment.NewLine}{fish.CloneV2()}");
+
+
+
+            //var goldfish = new GoldFish(3, "animal3");
+
+            //Console.WriteLine($"Original Item :{ Environment.NewLine}{goldfish}");
+            //Console.WriteLine($"Cloned Item1 :{ Environment.NewLine}{goldfish.CloneV2()}");
+
 
 
 
